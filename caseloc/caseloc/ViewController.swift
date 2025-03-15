94c9e34b53a67ef3d1f964294b020976247eaf47
Toygun Çil <toyguncil1@gmail.com>
2025-03-16 00:04:25 +0300

    create project

---
+++ caseloc/caseloc/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  caseloc
+//
+//  Created by Toygun Çil on 16.03.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ caseloc/caselocTests/caselocTests.swift
@@ -0,0 +1,17 @@
+//
+//  caselocTests.swift
+//  caselocTests
+//
+//  Created by Toygun Çil on 16.03.2025.
+//
+
+import Testing
+@testable import caseloc
+
+struct caselocTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
